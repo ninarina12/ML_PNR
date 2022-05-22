67c9f0a6fb3e0f00be7a3a05404abe27b9d04be6
Nina Andrejevic <nina12@atom.mit.edu>
2022-05-22 17:52:34 -0400

    Add Colab compatibility.

--- utils/utils_data.py
+++ utils/utils_data.py
@@ -10,7 +10,9 @@ from tqdm import tqdm
 # format progress bar
 bar_format = '{l_bar}{bar:10}{r_bar}{bar:-10b}'
 
+
 u = 1.66054 # conversion constant for density calculation
+
 
 def parse_metadata(dir_name):
 	# extract relevant parameters from metadata file
